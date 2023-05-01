5f40ab40df0a4e1b6cee9d6bff7146e857b06621
hnlp1997 <hnluupham@gmail.com>
2023-04-30 22:56:47 -0700

    fixed about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,6 +9,7 @@ export default function About() {
   return (
     <Layout>
       <div className={utilStyles.container}>
+        <div className='overflow-y-clip'>
         <Head>
           <link
             rel="icon"
@@ -25,30 +26,59 @@ export default function About() {
           <h1 className="text-4xl font-bold flex flex-col w-full mb-12 text-left lg:text-center pt-8">
             About Me
           </h1>
-          <div className="grid grid-cols-2 max-w-5xl items-center">
-            <div className="">
-              <p>
-                My name is <div class="relative overflow-hidden group"><span class="invisible">Hoang-Nam LuuPham</span><span class="absolute top-0 left-0 group-hover:-translate-y-full transition-transform ease-in-out duration-500 hover:duration-300">Hoang-Nam LuuPham</span><span class="absolute top-0 left-0 translate-y-full group-hover:translate-y-0 transition-transform ease-in-out duration-500 hover:duration-300">Hoang-Nam LuuPham</span></div>. I'm 21 years old, and I recently graduated from Santa Clara University with a BS in Business Management.
-                I will then be pursuing a Master's in Business Analytics full-time at SCU starting in Fall 2023. My goal post-graduation is to become a data analyst, as I enjoy the process of transforming messy, unformatted data
-                into valuable insights for company stakeholders. 
+          <div className="grid grid-cols-2 max-w-5xl items-start">
+            <div className="prose w-full text-left dark:prose-invert">
+              <p className="">
+                My name is <span class="font-bold">Nam</span>, nice to meet you! I’m 21 years old and am
+                currently pursuing my Master's in Business Analytics at Santa
+                Clara University. This summer, I will be interning at E. & J.
+                Gallo Winery as an IT Systems Analyst. I'm passionate about the
+                world of data analytics, as I enjoy transforming messy,
+                unformatted data into valuable insights!
               </p>
-
-              <h2 className="relative after:absolute after:bg-gray-200 after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300">
+              <p className="flex underline">
+                Here are some fun facts about me:
+              </p>
+              <ul className="list-disc list-inside indent-4">
+              
+                <li> 
+                  I DJ every Tuesday from 10-11 AM PST on{" "}
+                  <a href="https://kscu.org/" target="_blank">
+                    {" "}
+                    kscu.org
+                  </a>{" "}
+                </li>
+                <li>
+                  My younger brother Ha also attends SCU (sophomore)
+                </li>
+                <li>
+                  "LuuPham" is a combination of our parents' last names
+                </li>
+                <li>
+                  No matter what, I have to walk at least 10k+ steps daily
+                </li>
+                <li>
+                  Favorite artists: G Jones, DECO*27, TWICE, Virtual Self
+                </li>
+                <li>
+                  Favorite anime: K-ON!, NGE, Ping Pong, Kaguya-sama
+                </li>
                 
-              </h2>
-             
+              </ul>
 
-             
+              <h2 className="relative after:absolute after:bg-gray-200 after:bottom-0 after:left-0 after:h-[2px] after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300"></h2>
             </div>
             <img
               src="/hi.jpeg"
               alt="Hoang-Nam LuuPham"
-              height="350"
-              width="350"
+              height="475"
+              width="475"
               className="text-center mx-auto transform rounded-lg"
             />
           </div>
+       
         </main>
+        </div>
       </div>
     </Layout>
   );
